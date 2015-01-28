a62503859cfa0fa37f229786604294faa67c198d
bioid <bioshock@gmail.com>
2015-01-28 02:29:20 -0800

    switch to websocket-driver-node for handling the ws protocol

--- server.js
+++ server.js
@@ -1,111 +1,69 @@
 var net = require('net'),
-    crypto = require('crypto'),
     parser = require('http-string-parser'),
-    config = require('./config.js'),
-    frame = require('./frame/frame.js');
+    websocket = require('websocket-driver'),
+    config = require('./config.js');
+
 
 var server = net.createServer(function(socket) {
   // socket.setEncoding('ascii');
-      console.log('new connection');
+  console.log('new connection');
+  var driver = websocket.server({'protocols':'binary'});
+  socket.isWebSocket = false;
 
-      var isWebSocket = false;
-      var client = net.connect({host: config.TCP_SERVER.HOST, port: config.TCP_SERVER.PORT}, function() {
-          client.on('data', function(data) {
-            // data from the TCP server - send to the client
-            if (!isWebSocket) {
-              socket.write(data);
-              console.log('tcp - data from server forwarded to client');
-            }
-            else {
-              // construct the websocket frame to send here
-              binaryFrame = frame.createBinaryFrame(data, false, true, true);
-              socket.write(binaryFrame);
-            }
-          });
-      });
-      
-      socket.on('data', function(data) {
-        // data from the client - send to the tcp server
-        var request = parser.parseRequest(data.toString());
-        //console.log('request: ', request);
-
-        if (Object.keys(request.headers).length > 0) {
-          // HTTP
-         if (request.headers.Upgrade && request.headers.Upgrade == 'websocket') {
-          // WEBSOCKET CONNECTION
-          isWebSocket = true;
-          console.log('ws - websocket connection detected');
-          // Create the Sec-WebSocket-Accept header for our reponse
-          // (WEB SOCKET GUID) 258EAFA5-E914-47DA-95CA-C5AB0DC85B11
-          var guid = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11';
-          var shasum = crypto.createHash('sha1');
-          shasum.update(request.headers['Sec-WebSocket-Key'] + guid, 'string');
-          hash = shasum.digest('base64');
-
-          // Send the handshake
-          socket.write( "HTTP/1.1 101 Switching Protocols\r\n"
-                + "Upgrade: WebSocket\r\n"
-                + "Connection: Upgrade\r\n"
-                + "WebSocket-Origin: http://"+ request.headers.host +"\r\n"
-                + "WebSocket-Location: ws://"+ request.headers.host +"\r\n"
-                + "Sec-WebSocket-Accept: " + hash + "\r\n"
-                + "Sec-WebSocket-Protocol: binary\r\n"
-                + "\r\n"
-                );
-          console.log('ws - handshake sent');
-
-          socket.ondata = function (data, start, end) {
-            // Parse the websocket frame
-            // frame parsing courtesy of Logan Smythe's answer on stackoverflow:
-            // http://stackoverflow.com/a/14515142
-            var message = data.slice(start, end);
-            var FIN = (message[0] & 0x80);
-            var RSV1 = (message[0] & 0x40);
-            var RSV2 = (message[0] & 0x20);
-            var RSV3 = (message[0] & 0x10);
-            var Opcode = message[0] & 0x0F;
-            var mask = (message[1] & 0x80);
-            var length = (message[1] & 0x7F);
-
-            var nextByte = 2;
-            if (length === 126) {
-              // length = next 2 bytes
-              nextByte += 2;
-            } else if (length === 127){
-              // length = next 8 bytes
-              nextByte += 8;
-            }
-
-            var maskingKey = null;
-            if (mask){
-              maskingKey = message.slice(nextByte, nextByte + 4);
-              nextByte += 4;
-            }
-
-            var payload = message.slice(nextByte, nextByte + length);
-
-            if (maskingKey){
-              for (var i = 0; i < payload.length; i++){
-                payload[i] = payload[i] ^ maskingKey[i % 4];
-              }
-            }
-
-            console.log('ws - parsed message:', payload.toString());
-
-            // create a tcp connection and forward the data
-            client.write(payload);
-            console.log('ws - forwarded websocket message to tcp server');
-          };
+  var client = net.connect({host: config.TCP_SERVER.HOST, port: config.TCP_SERVER.PORT}, function() {
+    // client is the connection to the destination TCP server
+      client.on('data', function(data) {
+        // data from the TCP server - send to the client
+        if (!socket.isWebSocket) {
+          // our client is connected through tcp, send the data straight through
+          socket.write(data);
+          console.log('tcp - data from server forwarded to client');
         }
-      }
         else {
-          // THIS IS RAW TCP - FORWARD IT AS IS
-          console.log('tcp - message from client:', data);
-          client.write(data);
-          console.log('tcp - forwarded data to server');
+          // our client is connection through ws, send the data with ws
+          driver.binary(data);
+          console.log('ws - data from server forwarded to client');
         }
       });
+  });
 
+  driver.on('connect', function() {
+    // if the connection is a websocket, let the driver handle it
+    if (websocket.isWebSocket(driver)) {
+      socket.isWebSocket = true;
+      driver.start();
+      console.log('websocket conn');
+    }
+    else{ console.log(driver); }
+  });
+
+  socket.on('data', function(data) {
+    var request = parser.parseRequest(data.toString());
+    // console.log(request);
+    console.log('headers', Object.keys(request.headers).length)
+    if (Object.keys(request.headers).length === 0 && !socket.isWebSocket) {
+      // TCP
+      // THIS IS RAW TCP - FORWARD IT AS IS
+      console.log('tcp - message from client:', data);
+      client.write(data);
+      console.log('tcp - forwarded data to server');
+    }
+  });
+
+  driver.on('close', function(ev) {
+    socket.end();
+  });
+
+  driver.on('message', function(ev) {
+    console.log(ev);
+    client.write(ev.data);
+  });
+
+  socket.on('error', function(ev) { console.log(ev); });
+
+  socket.pipe(driver.io).pipe(socket);
+
+  driver.messages.pipe(driver.messages);
 });
 
 server.listen(config.LISTEN_PORT);
