45d91bbbcb8cc6802f688cbaa4b3888bc29326dd
bioid <bioshock@gmail.com>
2015-01-31 16:14:13 -0800

    remove try/catch because it doesn't work for node anyway

--- server.js
+++ server.js
@@ -31,108 +31,93 @@ var logHex = function(data, conn) {
 }
 
 var server = net.createServer(function(socket) {
-  try {
-    var firstPacket = true;
-    socket.isWebSocket = false;
-    var driver = websocket.server({'protocols':'binary'});
+  var firstPacket = true;
+  socket.isWebSocket = false;
+  var driver = websocket.server({'protocols':'binary'});
 
-    var tcpconn = net.connect({host: config.TCP_SERVER.HOST, port: config.TCP_SERVER.PORT}, function() {
-      // tcpconn is the connection to the destination TCP server
-        tcpconn.on('data', function(data) {
-          // data from the TCP server - send to the client
-          if (argv.debug) {
-            logHex(data, 'client');
-          }
-          if (!socket.isWebSocket) {
-            // our client is connected through tcp, send the data straight through
-            socket.write(data);
-          }
-          else {
-            // our client is connection through ws, send the data with ws
-            driver.binary(data);
-          }
-        });
-        tcpconn.on('close', function(ev) {
-          if (argv.debug && argv.debug != 'hexonly') {
-            console.log('socket to server closed');
-          }
-          tcpconn.end();
-        });
-        tcpconn.on('error', function(ev) {
-          console.log(ev);
-          tcpconn.destroy();
-          socket.destroy();
-        });
-    });
-
-    driver.on('connect', function() {
-      // if the connection is a websocket, let the driver handle it
-      if (websocket.isWebSocket(driver)) {
-        socket.isWebSocket = true;
-        driver.start();
-      }
-    });
-
-    socket.on('data', function(data) {
-      if (firstPacket) {
-        console.log('first packet');
-        firstPacket = false;
-        var request = parser.parseRequest(data.toString());
-        if (Object.keys(request.headers).length === 0 && !socket.isWebSocket) {
-          tcpconn.write(data);
-          if (argv.debug) { logHex(data, 'server'); }
+  var tcpconn = net.connect({host: config.TCP_SERVER.HOST, port: config.TCP_SERVER.PORT}, function() {
+    // tcpconn is the connection to the destination TCP server
+      tcpconn.on('data', function(data) {
+        // data from the TCP server - send to the client
+        if (argv.debug) {
+          logHex(data, 'client');
         }
-      }
-      else if (!firstPacket && !socket.isWebSocket) {
-        tcpconn.write(data)
+        if (!socket.isWebSocket) {
+          // our client is connected through tcp, send the data straight through
+          socket.write(data);
+        }
+        else {
+          // our client is connection through ws, send the data with ws
+          driver.binary(data);
+        }
+      });
+      tcpconn.on('close', function(ev) {
+        if (argv.debug && argv.debug != 'hexonly') {
+          console.log('socket to server closed');
+        }
+        tcpconn.end();
+      });
+      tcpconn.on('error', function(ev) {
+        console.log(ev);
+        tcpconn.destroy();
+        socket.destroy();
+      });
+  });
+
+  driver.on('connect', function() {
+    // if the connection is a websocket, let the driver handle it
+    if (websocket.isWebSocket(driver)) {
+      socket.isWebSocket = true;
+      driver.start();
+    }
+  });
+
+  socket.on('data', function(data) {
+    if (firstPacket) {
+      console.log('first packet');
+      firstPacket = false;
+      var request = parser.parseRequest(data.toString());
+      if (Object.keys(request.headers).length === 0 && !socket.isWebSocket) {
+        tcpconn.write(data);
         if (argv.debug) { logHex(data, 'server'); }
       }
-    });
-
-    driver.on('message', function(ev) {
-      tcpconn.write(ev.data);
-      if (argv.debug) {
-        logHex(ev.data, 'server');
-      }
-    });
-
-    driver.on('close', function(ev) {
-      socket.destroy();
-      tcpconn.destroy();
-      if (argv.debug && argv.debug != 'hexonly') {
-        console.log('websocket closed');
-      }
-    });
-
-    socket.on('close', function(ev) {
-      if (argv.debug && argv.debug != 'hexonly') {
-        console.log('socket to client closed');
-      }
-      socket.destroy();
-      tcpconn.destroy();
-    });
-
-    tcpconn.on('close', function(ev) {
-      tcpconn.destroy();
-      socket.destroy();
-    });
-    socket.on('error', function(ev) { 
-      console.log(ev);
-      socket.destroy();
-      tcpconn.destroy();
-    });
-
-    socket.pipe(driver.io).pipe(socket);
-  }
-  catch (err) {
-    console.log(err);
-    console.log('ERROR DETECTED - SHUTTING DOWN THE SOCKET');
-    socket.destroy();
-    if (tcpconn) {
-      tcpconn.destroy();
     }
-  }
+    else if (!firstPacket && !socket.isWebSocket) {
+      tcpconn.write(data)
+      if (argv.debug) { logHex(data, 'server'); }
+    }
+  });
 
+  driver.on('message', function(ev) {
+    tcpconn.write(ev.data);
+    if (argv.debug) {
+      logHex(ev.data, 'server');
+    }
+  });
+
+  driver.on('close', function(ev) {
+    socket.destroy();
+    tcpconn.destroy();
+    if (argv.debug && argv.debug != 'hexonly') {
+      console.log('websocket closed');
+    }
+  });
+
+  socket.on('close', function(ev) {
+    if (argv.debug && argv.debug != 'hexonly') {
+      console.log('socket to client closed');
+    }
+    socket.destroy();
+    tcpconn.destroy();
+  });
+
+  socket.on('error', function(ev) { 
+    console.log(ev);
+    socket.destroy();
+    tcpconn.destroy();
+  });
+
+  socket.pipe(driver.io).pipe(socket);
 });
 
 server.listen(config.LISTEN_PORT);
